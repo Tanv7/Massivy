5765c89164168fbb2acdf6bb13ad15f8d684750b
Массивы <tanv7@yandex.ru>
2024-04-02 20:18:54 +0300

    ДЗ массивы

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+
+        public class Point{
+        String name;
+        extends Point2D;
+        implements Serializable;
+        String math,object;
+        String geometric;
+
+                String color
+
+    }
+
+}
